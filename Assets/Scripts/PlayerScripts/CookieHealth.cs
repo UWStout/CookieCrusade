a2dd0a09ab3782b857d467a57c8101a183b9123b
Michael <hospm6044@my.uwstout.edu>
2020-02-21 15:12:22 -0600

    Revert "Revert "Please work""
    This reverts commit e53f864952d970c16080485bc41067fda57bc433.

--- Assets/Scripts/PlayerScripts/CookieHealth.cs
+++ Assets/Scripts/PlayerScripts/CookieHealth.cs
@@ -10,6 +10,11 @@ public class CookieHealth : MonoBehaviour
     [SerializeField]
     private HeartUI mHeartUI;
 
+    [SerializeField]
+    private int mRegenTime = 30;
+    [SerializeField]
+    private float mTimeLeft;
+
     public int HitPoints { get => mHitPoints; }
 
     // Start is called before the first frame update
@@ -24,6 +29,22 @@ public class CookieHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(mHitPoints <= 0)
+        {
+            GetComponent<Animator>().SetBool("hasDied", true);
+            //Extra Game Over Functionality
+        }
+
+        if(mHitPoints < 5)
+        {
+            mTimeLeft -= Time.deltaTime;
+            if(mTimeLeft < 0)
+            {
+                DealDamage(-1);
+            }
+        }
+        
+
         
     }
 
@@ -31,5 +52,7 @@ public class CookieHealth : MonoBehaviour
     {
         mHitPoints -= hitPoints;
         mHeartUI.DealDamage(hitPoints);
+        mTimeLeft = mRegenTime;
+        
     }
 }
